fadbbb2a4fab8f2c1eb15903872f06ba684f400e
radin-111 <radinsheikh87@gmail.com>
2026-02-02 15:10:41 +0600

    adding fetcOptions

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,10 @@
-
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
+import { includes } from "zod";
 
 export const authClient = createAuthClient({
-  baseURL:env.NEXT_PUBLIC_BACKEND_URL, 
+  baseURL: env.NEXT_PUBLIC_BACKEND_URL,
+  fetchOptions: {
+    credentials: "include",
+  },
 });
