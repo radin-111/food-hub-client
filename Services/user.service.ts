c423f5f0102ed558bf0203ed94cd46bb623bec88
radin-111 <radinsheikh87@gmail.com>
2026-02-02 15:32:46 +0600

    making a little change

--- Services/user.service.ts
+++ Services/user.service.ts
@@ -7,13 +7,14 @@ export const userService = {
   getSession: async function () {
     try {
       const cookieStore = await cookies();
-
+      const cookieHeader = await cookieStore.toString();
       const res = await fetch(`${AUTH_URL}/get-session`, {
         headers: {
-          Cookie: cookieStore.toString(),
+          Cookie: cookieHeader,
         },
-        cache: "no-store",
+
         credentials: "include",
+        cache: "no-store",
       });
 
       const session = await res.json();
@@ -31,12 +32,13 @@ export const userService = {
   getUsers: async function (page: string) {
     try {
       const cookieStore = await cookies();
+      const cookieHeader = await cookieStore.toString();
       const users = fetch(`${BACKEND_URL}/users?page=${page}`, {
-        cache: "no-store",
         headers: {
-          Cookie: cookieStore.toString(),
+          Cookie: cookieHeader,
         },
         credentials: "include",
+        cache: "no-store",
       });
       return users.then((res) => res.json());
     } catch (err) {
